0198e7204bfc259e277bf26b8f37bba85be8add2
ZazuUnzu <77108548+ZazuUnzu@users.noreply.github.com>
2021-06-13 22:20:34 +0200

    Delete server directory

--- server/src/index.js
+++
@@ -1,6 +0,0 @@
-require('./database')
-const app = require('./app')
-
-
-app.listen(app.get('port')); //Llamamos a la variable "port" que hemos creado en app.js
-console.log(`Server on port ${app.get('port')}`)
